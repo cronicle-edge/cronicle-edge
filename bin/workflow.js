67b1aef6e7a92149a8ab5e8ac956c8cf5cb3395a
miketwc1984 <miketwc1984@gmail.com>
2023-02-13 21:32:53 -0500

    WF plugin - wait for finishing jobs

--- bin/workflow.js
+++ bin/workflow.js
@@ -5,7 +5,7 @@ const PixlRequest = require('pixl-request');
 const request = new PixlRequest();
 const he = require('he');
 
-if(!process.env['WF_SIGNATURE']) throw new Error('WF Signature is not set')
+if (!process.env['WF_SIGNATURE']) throw new Error('WF Signature is not set')
 request.setHeader('x-wf-signature', process.env['WF_SIGNATURE'])
 request.setHeader('x-wf-id', process.env['JOB_ID'])
 
@@ -16,13 +16,14 @@ let baseUrl = process.env['BASE_URL'] || 'http://localhost:3012'
 
 let taskList = []  // todo list
 let jobStatus = {}  // a map of launched jobs
+let finishingJobs = {} // a map for 
 
 let errorCount = 0
 let wfStatus = 'running'
 let max_errors = parseInt(process.env['WF_MAXERR']);
 
 function niceInterval(s, asLocatTime) {
-	let date = new Date(s*1000);
+	let date = new Date(s * 1000);
 	//date.setSeconds(parseInt(s));
 	if (asLocatTime) return date.toLocaleTimeString();
 	return date.toISOString().substr(11, 8);
@@ -44,7 +45,7 @@ async function abortPending() {
 	for (let j in jobStatus) {
 		try {
 			if (!(jobStatus[j].completed)) {
-				let resp = await getJson(baseUrl + '/api/app/abort_job', { id: j})
+				let resp = await getJson(baseUrl + '/api/app/abort_job', { id: j })
 				console.log('SIGTERM sent to job ' + j);
 			}
 		}
@@ -76,35 +77,35 @@ rl.on('line', function (line) {
 
 	let wf_strict = parseInt(process.env['WF_STRICT']) || 0 // report error on any job failure (warning is default)
 
-	taskList = (input.workflow || []).map((e, i) => { e.stepId = i + 1; return e})
+	taskList = (input.workflow || []).map((e, i) => { e.stepId = i + 1; return e })
 
 	let opts = input.options || {}
 
 	let startFrom = opts.wf_start_from_step || 1
-	if ( startFrom > taskList.length) throw new Error('"Start From" parameter cannot exceed event list length')
+	if (startFrom > taskList.length) throw new Error('"Start From" parameter cannot exceed event list length')
 
 	let skip = taskList
-	    .filter((e, i) => (i + 1 < startFrom ) || !!e.disabled)
-	    .map(e => `[${e.stepId}] ${e.title} ${e.arg ? '@' + e.arg : ''}`)
-    
-    // exclude disabled/skipped jobs, form final task list
+		.filter((e, i) => (i + 1 < startFrom) || !!e.disabled)
+		.map(e => `[${e.stepId}] ${e.title} ${e.arg ? '@' + e.arg : ''}`)
+
+	// exclude disabled/skipped jobs, form final task list
 	taskList = taskList.filter((e, i) => i + 1 >= startFrom && !e.disabled)
-    // adjust concurrency level if needed
-	if(concur > taskList.length) concur = taskList.length
+	// adjust concurrency level if needed
+	if (concur > taskList.length) concur = taskList.length
 
 	/// sanity check
 	if (taskList.length == 0) throw new Error('At least one workflow event is required');
 	if (taskList.filter(e => e.id == process.env['JOB_EVENT']).length > 0) throw new Error("Workflow refers to itself");
 
 	async function poll() {
-        
+
 		// get a list of all scheduled jobs to look up event title
 		let sched = await getJson(baseUrl + '/api/app/get_schedule') //api_key: apikey,
 		let schedTitles = {};
 		(sched.data.rows || []).forEach(e => {
-			schedTitles[e.id] = e.title 
+			schedTitles[e.id] = e.title
 		});
-		taskList.forEach(e => {e.title = schedTitles[e.id] || '[Unknown]' })
+		taskList.forEach(e => { e.title = schedTitles[e.id] || '[Unknown]' })
 
 		let r = await getJson(baseUrl + '/api/app/get_active_jobs')
 		let currActive = []
@@ -115,8 +116,8 @@ rl.on('line', function (line) {
 
 		let lineLen = 0;
 		taskList.forEach(e => {
-			let msg = ` [${e.stepId}] ${e.title} (${e.id}) ${e.arg ? ('@' + e.arg) : ''}` 
-			if ( currActive.includes(e.id)) msg += `⚠️ already in progress  `
+			let msg = ` [${e.stepId}] ${e.title} (${e.id}) ${e.arg ? ('@' + e.arg) : ''}`
+			if (currActive.includes(e.id)) msg += `⚠️ already in progress  `
 			lineLen = lineLen > msg.length ? lineLen : msg.length
 			console.log(msg)
 		});
@@ -126,7 +127,7 @@ rl.on('line', function (line) {
 		if (skip.length > 0) {
 			console.log(` SKIPPING(${skip.length}):\n` + skip.map(e => " " + e).join("\n"))
 			console.log(' ' + '─'.repeat(lineLen))
-		} 
+		}
 
 		console.log(`\n\n\u001b[1m\u001b[32mWorkflow Started\u001b[39m\u001b[22m @ ${(new Date()).toLocaleString()}\n │  `)
 
@@ -135,18 +136,18 @@ rl.on('line', function (line) {
 		// launch first batch of jobs
 		for (let q = 0; q < concur; q++) {
 			let task = taskList[q];
-			console.log(` \n ├───────> starting \u001b[1m${task.title}\u001b[22m${task.arg ? ': '+task.arg : ''}`);
+			console.log(` \n ├───────> starting \u001b[1m${task.title}\u001b[22m${task.arg ? ': ' + task.arg : ''}`);
 
 			try {
 				let job = await getJson(baseUrl + '/api/app/run_event', { id: task.id, arg: task.arg || 0 });
 				if (job.data.queue) throw new Error("Event has beed added to internal queue and will run independently from this WF")
-				jobStatus[job.data.ids[0]] = { 
-					  event: task.id
+				jobStatus[job.data.ids[0]] = {
+					event: task.id
 					, title: task.title
 					, arg: task.arg
 					, completed: false
 					, code: 0
-					, seq: q+1
+					, seq: q + 1
 					, start: (new Date()).toLocaleTimeString()
 					, rerunid: ' '
 				}
@@ -160,7 +161,7 @@ rl.on('line', function (line) {
 					arg: task.arg,
 					completed: false,
 					code: 1,
-					seq: q+1,
+					seq: q + 1,
 					start: (new Date()).toLocaleTimeString(),
 					description: e.message,
 					elapsed: 0
@@ -183,7 +184,7 @@ rl.on('line', function (line) {
 			let rerunList = {};
 			for (r in activeJobs) {
 				if (activeJobs[r].when) {
-					rerunList[activeJobs[r].id] = {id: r, when: activeJobs[r].when, retries: activeJobs[r].retries }
+					rerunList[activeJobs[r].id] = { id: r, when: activeJobs[r].when, retries: activeJobs[r].retries }
 				}
 			}
 
@@ -199,9 +200,8 @@ rl.on('line', function (line) {
 					continue
 				}
 
-				if (!(resp.data.jobs[j]) && !(rerunList[j]) ) {  // if job is not in active job list or rerun queue - mark as completed
-					jobStatus[j].completed = true
-					pendingJobs -= 1
+				if (!(resp.data.jobs[j]) && !(rerunList[j])) {  // if job is not in active job list or rerun queue - mark as completed
+
 					//if(rerunList[j]) console.log(`job ${j} is still running on background`)
 
 					let msg = '';
@@ -214,41 +214,38 @@ rl.on('line', function (line) {
 						// check job status
 						let jstat = "";
 						let desc = "  ";
-						
+
 						// retrieve completed job details. API call may fail due to network lag/error, WF will try it few more times before crashing.
 						let lag = 30
 						let jd = {}
-						while (!jd.data) {
-							try {
-								await sleep(lag);
-								jd = await getJson(baseUrl + '/api/app/get_job_details', { id: j })
-							}
-							catch {
-								if (lag > 500) {
-									console.log(" │ ⚠️ Failed to retreive data from get_job_details")
-									jd = {
-										data: {
-											job: {
-												code: 1,
-												description: "failed to retreive job state (check job logs)",
-												elapsed: 0
-											}
-										}
-									}
-									break
 
-								}
-								lag = lag*3								
+						try {
+							await sleep(lag);
+							jd = await getJson(baseUrl + '/api/app/get_job_details', { id: j })
+							// emulate error
+						}
+						catch (e) {
+							// you get here if job is no longer active, but still has no log stored. 
+							// in this case just indicate that ob is finishing up and keep waiting for log info.
+							if (finishingJobs[j]) { finishingJobs[j] += 1 }
+							else {
+								finishingJobs[j] = 1
+								console.log(` ├───────> Job ${j} is finishing up`)
 							}
-						}	
-						
+							// console.log(" │  ---- DEBUG: ----:", e.message) // just for testing
+							continue
+						}
+
+						jobStatus[j].completed = true
+						pendingJobs -= 1
+
 						let compl = jd.data.job;
 						if (compl) {
 							if (compl.code == 0) { jstat = '\u001b[32m⬤\u001b[39m' }
 							else if (compl.code == 255) { jstat = '\u001b[33m⬤\u001b[39m'; desc = `\n │    warn: \u001b[33m${compl.description}\u001b[39m  ` }
 							else {
 								errorCount += 1;
-								jstat = '\u001b[31m⬤\u001b[39m' ;
+								jstat = '\u001b[31m⬤\u001b[39m';
 								desc = `\n │    Error: \u001b[31m${compl.description}\u001b[39m  `
 								if (max_errors && errorCount >= max_errors) wfStatus = 'abort'; // prevent launching new jobs
 
@@ -258,41 +255,41 @@ rl.on('line', function (line) {
 							jobStatus[j].code = compl.code
 						}
 						let prog = `[${taskList.length - pendingJobs}/${taskList.length}]`
-						let arg = jobStatus[j].arg ? ': '+ jobStatus[j].arg : ''
+						let arg = jobStatus[j].arg ? ': ' + jobStatus[j].arg : ''
 						msg = ` │ ${jstat} ${jobStatus[j].title + arg} (job ${j}) completed ${prog} at ${(new Date()).toLocaleTimeString()}\n │      \u001b[33melapsed in ${niceInterval(jobStatus[j].elapsed)}\u001b[39m  ${desc}`
 					}
 
-					msg += '\n │'				
+					msg += '\n │'
 
 					// starting next job in queue
 					if (next < taskList.length && wfStatus != 'abort') {
 
 						let task = taskList[next]
 
-						msg += `\n ├───────> starting \u001b[1m${task.title}\u001b[22m${task.arg ? ': '+task.arg : ''}`;
-				
+						msg += `\n ├───────> starting \u001b[1m${task.title}\u001b[22m${task.arg ? ': ' + task.arg : ''}`;
+
 						try {
 							let job = await getJson(baseUrl + '/api/app/run_event', { id: task.id, arg: task.arg || 0 });
 							if (job.data.queue) throw new Error("Event has beed added to internal queue and will run independently from this WF")
-							jobStatus[job.data.ids[0]] = { 
-								  event: task.id
+							jobStatus[job.data.ids[0]] = {
+								event: task.id
 								, arg: task.arg
 								, title: task.title
 								, completed: false
 								, code: 0
-								, seq: next+1
+								, seq: next + 1
 								, start: (new Date()).toLocaleTimeString()
-							 }
+							}
 						}
 						catch (e) {
 							errorCount += 1;
 							jobStatus[task.id] = {
-								  event: task.id
+								event: task.id
 								, arg: task.arg
 								, title: task.title
 								, completed: false
 								, code: 1
-								, seq: next+1
+								, seq: next + 1
 								, start: (new Date()).toLocaleTimeString()
 								, description: e.message
 								, elapsed: 0
@@ -332,7 +329,7 @@ rl.on('line', function (line) {
 			header: [
 				"#", "title", "job", "started at", "elapsed", "status", "description"
 			],
-			rows: Object.keys(jobStatus).map(key=> [
+			rows: Object.keys(jobStatus).map(key => [
 				jobStatus[key].seq,
 				`<span style="${jobStatus[key].code % 255 ? 'color:red' : ''}"><b>${he.encode(jobStatus[key].title) || '[Unknown]'}</b></span>` + (jobStatus[key].arg ? ': ' + he.encode(jobStatus[key].arg) : ''),
 				key === jobStatus[key].event ? '' : `<a href="/#JobDetails?id=${key}" target="_blank">${key}</a>`,
@@ -340,7 +337,7 @@ rl.on('line', function (line) {
 				niceInterval(jobStatus[key].elapsed),
 				jobStatus[key].code ? (jobStatus[key].code == 255 ? '<span style="color:orange"><b>⚠️</b></span>' : '<span style="color:red"><b>✗</b></span>') : '<span style="color:green"><b>✔</b></span>',
 				//jobStatus[key].code ? `${he.encode(jobStatus[key].description)}`.substring(0,120) : ''
-				`${he.encode(jobStatus[key].description)}`.substring(0,120)
+				`${he.encode(jobStatus[key].description)}`.substring(0, 120)
 
 			]),
 			caption: ""
