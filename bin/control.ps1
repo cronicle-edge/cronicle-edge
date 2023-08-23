3a36d012290915a724cb91744a6ae934e4e7f8f6
Mike <miketwc1984@gmail.com>
2023-08-23 00:58:11 -0400

    control.ps1 ls update

--- bin/control.ps1
+++ bin/control.ps1
@@ -14,7 +14,34 @@ param(
 
 $ErrorActionPreference = "stop"
 
-$conf = Get-Content "$PSScriptRoot\..\conf\config.json" | ConvertFrom-Json -Depth 10
+if (!$Command) {
+    Write-Host "Usage:
+
+    .\control.ps1 start [-Manager] [-Echo] [-Force] [-Color] [-WinMon] [-Hide]
+         Force: force stop cronicle if running already (Restart)
+         WinMon: enable windows event monitor (to catch Windows restart for graceful shutdown)
+         Manager: become a manager immediatly (no wait for 60 seconds)
+         Echo: print cronicle logs in console
+         Hide: start cronicle on the background (this will prevent graceful shutdown, unless you do it from cronicle UI
+    .\control.ps1 start -CronicleArgs @('--echo', '--manager', ...) # pass a custom set of parameters to cronicle.js
+
+    .\control.ps1 stop [-Force] # stop cronicle. If running in the background can only stop with -Force (non-graceful shutdown)
+
+    .\control.ps1 status ### check if cronicle is running
+
+    .\control.ps1 logs [-Tail 20]  ## check Cronicle.log records
+
+    .\control.ps1 ls  events|jobs|servers|server_groups|api_keys|conf_keys|secrets|cats|logs [-Page 0]  # browse cronicle storage
+         Page: use it if collection has multiple pages (e.g. > 50 records). By default Page == 0
+      examples:
+      .\control.ps1 ls events | sort -Property mod -Descending | ft
+      .\control.ps1 ls events | sort -Property plugin -Descending | ft -GroupBy plugin
+      .\control.ps1 ls jobs -Page -1 | select -first 20 | ft
+
+     
+"
+}
+# $conf = Get-Content "$PSScriptRoot\..\conf\config.json" | ConvertFrom-Json -Depth 10
 
 
 # -------------------- check if process is up
@@ -44,7 +71,7 @@ if ($Command -eq "start") {
     if ($manager.IsPresent) { [void]$args.Add("--manager") }
     if ($Color.IsPresent) { [void]$args.Add("--color") }
 
-    if($Force.IsPresent) {
+    if ($Force.IsPresent) {
         $proc | Stop-Process
         $proc = $null
     }
@@ -70,7 +97,7 @@ if ($Command -eq "start") {
 
 if ($Command -eq "stop") {
 
-    if(!$proc) {
+    if (!$proc) {
         Write-Host "Cronicle is not running"; exit 0;
     }
 
@@ -102,57 +129,70 @@ if ($Command -eq "logs") {
     exit 0
 }
 
-if($Command -in "list", "ls") {
-    if(!$Key) {
+if ($Command -in "list", "ls") {
+    if (!$Key) {
         Write-Host "Specify items to list:"
-        Write-Host "[events|servers|server_groups|api_keys|conf_keys|secrets|cats|logs]`n"
+        Write-Host "[events|jobs|servers|server_groups|api_keys|conf_keys|secrets|cats|logs]`n"
         exit 1
     }
         
-    if($Key -eq "events") { $Key = "schedule" }
-    if($Key -eq "cats") { $Key = "categories" }
-     $list = "global/$Key/$Page"
-    if($Key -eq "logs") { $list = "logs/activity/$Page" }
+    if ($Key -eq "events") { $Key = "schedule" }
+    if ($Key -eq "cats") { $Key = "categories" }
+    $list = "global/$Key"
+    if ($Key -eq "logs") { $list = "logs/activity" }
+    if ($Key -eq "jobs") { $list = "logs/completed" }
     
-    $timing = @{n="schedule"; e = {      
-        if($_.timing.years) { "custom"}
-        elseif ($_.timing.months) {"yearly" }
-        elseif ($_.timing.days) {"monthly"}
-        elseif ($_.timing.weekdays) {"weekly"}
-        elseif ($_.timing.hours) {"daily" }
-        elseif ($_.timing.minutes) {"hourly" }
-        else {"on demand" }
-      } 
+    $timing = @{n = "schedule"; e = {      
+            if ($_.timing.years) { "custom" }
+            elseif ($_.timing.months) { "yearly" }
+            elseif ($_.timing.days) { "monthly" }
+            elseif ($_.timing.weekdays) { "weekly" }
+            elseif ($_.timing.hours) { "daily" }
+            elseif ($_.timing.minutes) { "hourly" }
+            else { "on demand" }
+        } 
     }
 
     $global:cats = @{}
     $global:plugs = @{}
-    if($Key -eq "schedule") {
+    if ($Key -eq "schedule") {
         node $PSScriptRoot\storage-cli.js get 'global/categories/0' | convertfrom-json | % items | % { $cats[$_.id] = $_.title }
         node $PSScriptRoot\storage-cli.js get 'global/plugins/0' | convertfrom-json | % items | % { $plugs[$_.id] = $_.title }
     }
 
-    $mod = @{name = "mod"; e = {[System.DateTimeOffset]::FromUnixTimeSeconds($_.modified).ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss")}}
-    $epoch = @{name = "epoch"; e = {[System.DateTimeOffset]::FromUnixTimeSeconds($_.epoch).ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss")}}
-    $notes = @{name="notes"; e={$_.notes.Length -gt 40 ? $_.notes.Substring(0,40) + "..." : $_.notes}}
-    $cat = @{name="category"; e = {$cats[$_.category]}}
-    $plug = @{name="plugin"; e = {$plugs[$_.plugin]}}
+    $mod = @{name = "mod"; e = { [System.DateTimeOffset]::FromUnixTimeSeconds($_.modified).ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss") } }
+    $epoch = @{name = "epoch"; e = { [System.DateTimeOffset]::FromUnixTimeSeconds($_.epoch).ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss") } }
+    $notes = @{name = "notes"; e = { $_.notes.Length -gt 40 ? $_.notes.Substring(0, 40) + "..." : $_.notes } }
+    $cat = @{name = "category"; e = { $cats[$_.category] } }
+    $plug = @{name = "plugin"; e = { $plugs[$_.plugin] } }
 
     $propMap = @{
-        schedule = @( "title", "id", $cat, $plug, $timing, $mod, $notes )
-        logs = @("action", "username", "ip", $epoch )
-        api_keys = @("title", $mod, "privileges")
-        secrets = @("id", "encrypted", "form")
+        schedule   = @( "title", "id", $cat, $plug, $timing, $mod, $notes )
+        logs       = @("action", "username", "ip", $epoch )
+        api_keys   = @("title", $mod, "privileges")
+        secrets    = @("id", "encrypted", "form")
         categories = @("id", "title", "enabled", "description", $mod)
-        plugins = @("id", "title", "enabled", "command", $mod)
-        conf_keys = @("title", "key")
+        plugins    = @("id", "title", "enabled", "command", $mod)
+        conf_keys  = @("title", "key")
+        jobs       = @($epoch, "event_title", "category_title", "plugin_title", "elapsed", "code", 'description')
+
     }
     
     $props = $propMap[$Key] ?? "*"
     
-    try {
-    node $PSScriptRoot\storage-cli.js get $list | convertfrom-json | % items | select $props
-    } catch { Write-Host "No such collection"; exit 1}
+    $listMeta = node $PSScriptRoot\storage-cli.js get $list | convertfrom-json
+    $firstPage = $listMeta.first_page
+    $nextPage = $firstPage + 1
+    if ($Page) {
+        node $PSScriptRoot\storage-cli.js get "$list/$Page" | convertfrom-json | % items | select $props
+    }
+    else {
+        Write-Host "----------- List info: $($listMeta) -----------------------"
+        node $PSScriptRoot\storage-cli.js get "$list/$firstPage" | convertfrom-json | % items | select $props
+        if ($listMeta.length -gt 50) {
+            node $PSScriptRoot\storage-cli.js get "$list/$nextPage" | convertfrom-json | % items | select $props
+        }
+    }
 
     exit 0
 }
