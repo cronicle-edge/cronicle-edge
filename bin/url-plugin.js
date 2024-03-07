b65629cfaf2996f711445f3aa32aa61798585922
Luca Bernstein <luca@git.lucabernstein.com>
2024-03-07 08:58:15 +0100

    Fix rejectUnauthorized
    See fork origin fix: https://github.com/jhuckaby/Cronicle/pull/724

--- bin/url-plugin.js
+++ bin/url-plugin.js
@@ -74,7 +74,7 @@ stream.on('json', function(job) {
 
 	// ssl cert bypass
 	if (params.ssl_cert_bypass) {
-		opts.rejectUnauthorized = true;
+		opts.rejectUnauthorized = false;
 	}
 	
 	// post data
